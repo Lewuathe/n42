1ae48d7c26d4002e538ee03c6aff5026b2211374
Lewuathe <lewuathe@me.com>
2014-01-24 23:02:13 +0900

    Vector case

--- test/utils-test.js
+++ test/utils-test.js
@@ -3,26 +3,51 @@ var vows = require('vows'),
 
 var utils = require('../lib/utils.js');
 var Matrix = require('sylvester').Matrix;
+var Vector = require('sylvester').Vector;
 var _ = require('underscore');
 
 vows.describe('n42 Utils test').addBatch({
     'sigmoid function': {
-        topic: function() {
-            return utils.sigmoid($M([0.0, 0.0, 0.0]));
-        },
-
-        'should receive 0.5': function (topic) {
-            assert.deepEqual(topic, $M([0.5,0.5,0.5]));
+        'get correct answer': {
+            topic: function() {
+                return utils.sigmoid($M([0.0, 0.0, 0.0]));
+            },
+            
+            'should receive 0.5': function (topic) {
+                assert.deepEqual(topic, $M([0.5,0.5,0.5]));
+            }
         }
     },
 
     'softmax function': {
-        topic: function() {
-            return utils.softmax($M([[1.0, 1.0], [1.0, 1.0]]));
+        'get matrix which has 2 dimensions': {
+            topic: function() {
+                return utils.softmax($M([[1.0, 1.0], [1.0, 1.0]]));
+            },
+            
+            'should receive 0.5': function (topic) {
+                assert.deepEqual(topic, $M([[0.5, 0.5], [0.5, 0.5]]));
+            }
+        },
+        
+        'get matrix which has 1 dimensions': {
+            topic: function() {
+                return utils.softmax($M([[1.0, 1.0]]));
+            },
+
+            'should receive 0.5': function(topic) {
+                assert.deepEqual(topic, $M([[0.5, 0.5]]));
+            }
         },
 
-        'should receive 0.5': function (topic) {
-            assert.deepEqual(topic, $M([[0.5, 0.5], [0.5, 0.5]]));
+        'get vector': {
+            topic: function() {
+                return utils.softmax($V([1.0, 1.0]));
+            },
+
+            'should receive 0.5': function(topic) {
+                assert.deepEqual(topic, $V([0.5, 0.5]));
+            }
         }
     }
 
