2a69efda6e4ae71984809dc06ba14f21828ddb86
Lewuathe <lewuathe@me.com>
2014-01-26 16:18:37 +0900

    Hidden Layer

---
+++ test/HiddenLayer-test.js
@@ -0,0 +1,25 @@
+ vows = require('vows'),
+    assert = require('assert');
+
+var HiddenLayer = require('../lib/HiddenLayer.js');
+var utils  = require('../lib/utils.js');
+var Matrix = require('sylvester').Matrix;
+var Vector = require('sylvester').Vector;
+var _ = require('underscore');
+
+vows.describe('n42 HiddenLayer module').addBatch({
+    'Simple hidden layer': {
+        'get correct answer': {
+            topic: function() {
+                return new HiddenLayer($M([[1.0, 0.0, 1.0], [1.0, 1.0, 0.0], [0.0, 1.0, 1.0]]),
+                                       3, 2,
+                                      $M([[1.0, 1.0],[1.0, 1.0], [1.0, 1.0]]),$V([1.0, 1.0]));
+            },
+            
+            'should receive 0.5': function (hiddenLayer) {
+                assert.isNotNull((hiddenLayer.output($M([[1.0, 0.0, 1.0], [1.0, 0.0, 0.0]]))));
+            }
+        }
+    }
+
+}).export(module); // Export the Suite
