8174db2293dac8ed135e586b89cdb726f5d10cdb
Lewuathe <lewuathe@me.com>
2014-01-28 20:09:56 +0900

    Generate yuidocs

---
+++ docs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "LogisticsRegression",
+        "SdA"
+    ],
+    "modules": [
+        "n42"
+    ],
+    "allModules": [
+        {
+            "displayName": "n42",
+            "name": "n42",
+            "description": "Logistics Regression module."
+        }
+    ]
+} };
+});
