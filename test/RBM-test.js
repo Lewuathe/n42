ed14257ebe6c4e82685277095e0f2a32f8391584
Lewuathe <lewuathe@me.com>
2014-01-31 21:29:40 +0900

    Restricted boltzmann machine

---
+++ test/RBM-test.js
@@ -0,0 +1,80 @@
+ vows = require('vows'),
+    assert = require('assert');
+
+var RBM = require('../lib/RBM.js');
+var utils  = require('../lib/utils.js');
+var Matrix = require('sylvester').Matrix;
+var Vector = require('sylvester').Vector;
+var _ = require('underscore');
+
+vows.describe('n42 RBM  module').addBatch({
+    'Restricted Boltzmann machine': {
+        'get correct answer': {
+            topic: function() {
+
+                var input = $M([[1,1,1,0,0,0],
+                                [1,0,1,0,0,0],
+                                [1,1,1,0,0,0],
+                                [0,0,1,1,1,0],
+                                [0,0,1,1,0,0],
+                                [0,0,1,1,1,0]]);
+                            
+                
+                return new RBM(input, 6, 2);
+            },
+            
+            'should correct propup output': function (rbm) {
+                var input = $M([[1,1,1,0,0,0],
+                                [1,0,1,0,0,0],
+                                [1,1,1,0,0,0],
+                                [0,0,1,1,1,0],
+                                [0,0,1,1,0,0],
+                                [0,0,1,1,1,0]]);
+                
+                assert.isNotNull(rbm.propup(input));
+            },
+            'should correct propdown output': function(rbm) {
+                var input = $M([[1.0, 0.0],
+                                [1.0, 0.0],
+                                [0.0, 0.0],
+                                [0.0, 1.0],
+                                [0.0, 1.0],
+                                [0.0, 1.0]]);
+
+                assert.isNotNull(rbm.propdown(input));
+            },
+            'should correct sample h given v': function(rbm) {
+                var input = $M([[1,1,1,0,0,0],
+                                [1,0,1,0,0,0],
+                                [1,1,1,0,0,0],
+                                [0,0,1,1,1,0],
+                                [0,0,1,1,0,0],
+                                [0,0,1,1,1,0]]);
+                
+                var ret = rbm.sampleHGivenV(input)
+                assert.isNotNull(ret);
+            },
+            'should correct sample v given h': function(rbm) {
+                var input = $M([[1.0, 0.0],
+                                [1.0, 0.0],
+                                [0.0, 0.0],
+                                [0.0, 1.0],
+                                [0.0, 1.0],
+                                [0.0, 1.0]]);
+                var ret = rbm.sampleVGivenH(input);
+                assert.isNotNull(ret);
+            },
+
+            'should train propery': function(rbm) {
+                for (var i = 0; i < 1000; i++) {
+                    rbm.contrastiveDivergence(0.3, 1);
+                }
+                var v = $M([[1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 0.0, 1.0, 1.0, 1.0]]);
+                console.log(rbm.reconstruct(v));
+            }
+            
+        }
+    }
+
+}).export(module); // Export the Suite
