bb40056f1308eb70d1d95c673a777aab95e81e3e
Lewuathe <lewuathe@me.com>
2014-02-12 22:00:09 +0900

    Newral network

---
+++ test/NN-test.js
@@ -0,0 +1,46 @@
+vows = require('vows');
+assert = require('assert');
+
+var NN = require('../lib/NN.js');
+var utils = require('../lib/utils.js');
+var Matrix = require('sylvester').Matrix;
+var Vector = require('sylvester').Vector;
+
+vows.describe('n42 Newral network module').addBatch({
+    'Newral Network': {
+        'get correct answer': {
+            topic: function() {
+                var input = $M([
+                    [1.0, 1.0, 0.0, 0.0],
+                    [1.0, 1.0, 0.2, 0.0],
+                    [1.0, 0.9, 0.1, 0.0],
+                    [0.0, 0.0, 1.0, 1.0],
+                    [0.0, 0.1, 0.8, 1.0],
+                    [0.0, 0.0, 0.9, 1.0]
+                ]);
+
+                var label = $M([
+                    [1.0, 0.0],
+                    [1.0, 0.0],
+                    [1.0, 0.0],
+                    [0.0, 1.0],
+                    [0.0, 1.0],
+                    [0.0, 1.0]
+                ]);
+
+                return new NN(input, 4, 3, 2, label);
+            },
+
+            'should predict correctly': function(nn) {
+                for (var i = 0; i < 1000; i++) {
+                    nn.train(0.3);
+                }
+                var data = $M([
+                    [1.0, 1.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 1.0]
+                ]);
+                console.log(nn.predict(data));
+            }
+        }
+    }
+}).export(module);
